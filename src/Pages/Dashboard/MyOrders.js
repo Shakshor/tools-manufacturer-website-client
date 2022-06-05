4ceddfd39a1f82db491aeee839841f985c3256b0
Shakshor <shakshor584@gmail.com>
2022-06-05 06:25:03 +0600

    correcting input field and creating of order object

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyOrders = () => {
+    // react firebase hooks
+    const [user] = useAuthState(auth);
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/myItems?email=${user?.email}`)
+                .then(res => res.json())
+                .then(data => setOrders(data))
+        }
+
+    }, [user])
+
     return (
         <div>
-            <h2>My Orders..</h2>
+            <h2>My Orders..:{orders.length}</h2>
         </div>
     );
 };
--- src/Pages/Home/Purchase.js
+++ src/Pages/Home/Purchase.js
@@ -2,19 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
-import { toast } from 'react-toastify';
 
 const Purchase = () => {
-    const { productId } = useParams();
+    // react firebase hooks
     const [user] = useAuthState(auth);
+    const { productId } = useParams();
     const [tool, setTool] = useState({});
 
     const { _id, name, img, description, quantity, available, price } = tool;
-
-    const [order, setOrder] = useState('');
-    const [agree, setAgree] = useState(false);
-
-
+    // console.log(tool);
     useEffect(() => {
         const url = `http://localhost:5000/product/${productId}`;
         fetch(url)
@@ -26,70 +22,88 @@ const Purchase = () => {
     if (user) {
         // console.log(user);
     }
-    const handleSubmit = event => {
+
+    // quantity of order
+    const handleChange = event => {
+        const orderQuantity = event.target.value;
+    };
+
+    // 
+    const handleOrder = event => {
         event.preventDefault();
-        console.log(event);
+        // console.log(event);
         const phoneNumber = event.target.phone.value;
         const address = event.target.address.value;
-        // const orderQuantity = event.target.quantity.value;
-        // if (orderQuantity < { quantity } || orderQuantity > { available }) {
-        //     setAgree(false);
-        //     toast('Please Enter Valid Order');
-        // }
-        console.log(address, phoneNumber);
-    }
-    const handleOrder = event => {
         const orderQuantity = event.target.quantity.value;
-        if (orderQuantity < { quantity } || orderQuantity > { available }) {
-            setAgree(false);
-            toast('Please Enter Valid Order');
+        // console.log(address, phoneNumber, orderQuantity);
+
+        // sending order data to backend
+        // creating order object data
+        const order = {
+            orderId: _id,
+            order: name,
+            Quantity: orderQuantity,
+            user: user.email,
+            userName: user.displayName,
+            phoneNumber,
+            address,
         }
+
     }
+
+
 
     return (
         <div className='max-w-lg mx-auto'>
-            {/* ------ Provided Information of User------- */}
-            <div class="form-control w-full max-w-lg bg-base-100 my-10">
-                <h2 className='text-xl text-primary font-semibold text-center my-3 uppercase'>Please Fill The Form</h2>
-                <form onSubmit={handleSubmit}>
-                    <input type="email" value={user?.email} name='email' placeholder="Name" class="input input-bordered input-primary w-full max-w-lg my-2 text-base" required readOnly disabled />
-                    <input type="text" value={user?.displayName} name='name' placeholder="Name" class="input input-bordered input-primary w-full max-w-lg my-2 text-base" required readOnly disabled />
-                    <input type="text" name='address' placeholder="Address" class="input input-bordered input-primary w-full max-w-lg my-2 text-base" required />
-                    <input type="text" name='phone' placeholder="Phone Number" class="input input-bordered input-primary w-full max-w-lg my-2 text-base" required />
-                    <div>
-                        <button>+</button>
-                        <input type="text" value={order} onInput={event => setOrder(event.target.value)} name='quantity' placeholder="Order Quantity" class="input input-bordered input-primary w-full max-w-lg my-2 text-base" required />
-                        <button>-</button>
-                    </div>
-
-                    <div className='text-center'>
-
-                        <input type="submit" className='btn btn-primary btn-wide' value='Submit' />
-
-                    </div>
-                </form>
-            </div>
-
             {/* --- product info ------- */}
-
             <h2>Purchase the Item here:{productId}</h2>
-            <div class="card lg:max-w-lg  bg-base-100 shadow-xl">
-                <figure class="px-10 pt-10">
-                    <img src={img} alt="Shoes" class="rounded-xl" />
+            <div className="card lg:max-w-lg  bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src={img} alt="Shoes" className="rounded-xl" />
                 </figure>
-                <div class="card-body items-center text-center">
-                    <h2 class="card-title text-emerald-500">{name}</h2>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title text-emerald-500">{name}</h2>
                     <p >{description}</p>
                     <p className='text-base text-blue-700'>Price:{price}</p>
                     <p >Minimum Order:{quantity}</p>
                     <p >Available:{available}</p>
-                    <div class="card-actions">
-                        <button class="btn btn-primary btn-wide">Purchase</button>
-                    </div>
                 </div>
             </div>
 
 
+            {/* ------ Provided Information of User------- */}
+            <div className="form-control w-full max-w-lg bg-base-100 my-10">
+                <h2 className='text-2xl text-primary font-semibold text-center my-3 uppercase'>Please Fill The Form</h2>
+                <form onSubmit={handleOrder}>
+                    <input type="email" value={user?.email} name='email' placeholder="Name" className="input input-bordered input-primary w-full max-w-lg my-2 text-base" required readOnly disabled />
+                    <input type="text" value={user?.displayName} name='name' placeholder="Name" className="input input-bordered input-primary w-full max-w-lg my-2 text-base" required readOnly disabled />
+
+                    <div class="form-control w-full max-w-lg">
+                        <label class="label">
+                            <span class="label-text text-lg text-primary">Address</span>
+                        </label>
+                        <input type="text" name='address' placeholder="Address" className="input input-bordered input-primary w-full max-w-lg my-2 text-base" required />
+                    </div>
+
+                    <div class="form-control w-full max-w-lg">
+                        <label class="label">
+                            <span class="label-text text-lg text-primary">Phone</span>
+                        </label>
+                        <input type="text" name='phone' placeholder="Phone Number" className="input input-bordered input-primary w-full max-w-lg my-2 text-base" required />
+                    </div>
+
+                    <div class="form-control w-full max-w-lg">
+                        <label class="label">
+                            <span class="label-text text-lg text-primary">Order Quantity</span>
+                        </label>
+                        <input defaultValue={quantity} name='quantity' type="number" min={quantity} max={available} onChange={handleChange} className="input input-bordered input-primary text-center text-xl" placeholder={quantity} required />
+                    </div>
+
+                    <div className='text-center'>
+                        <input type="submit" className='btn btn-primary w-full max-w-lg mt-10' value='Purchase' />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
