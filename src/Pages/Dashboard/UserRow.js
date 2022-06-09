5712c1aea6ddf7d5bea6549b233b292f7f6f4289
Shakshor <shakshor584@gmail.com>
2022-06-09 18:25:48 +0600

    user role as an admin done

---
+++ src/Pages/Dashboard/UserRow.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+
+const UserRow = ({ user, refetch }) => {
+    const { email, role } = user;
+
+    // event handler for make admin
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                refetch();
+                toast.success(`Successfully made an admin`)
+            })
+    }
+
+    return (
+        <tr>
+            <th>1</th>
+            <td>{email}</td>
+            <td>{role !== 'admin' && <button className="btn btn-xs" onClick={makeAdmin}>Make Admin</button>}</td>
+            <td><button className="btn btn-xs" >Remove User</button></td>
+        </tr >
+    );
+};
+
+export default UserRow;
