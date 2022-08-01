7131220c161c4f2f2dceb5c30c3fe6a28c19a5c8
Shakshor <shakshor584@gmail.com>
2022-08-02 01:07:07 +0600

    banner problem solved

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -12,14 +12,14 @@ const Banner = () => {
                     <div className="hero-content text-center text-neutral-content">
                         <div className="max-w-md">
                             <h1 className="mb-5 text-3xl lg:text-5xl font-bold font-serif">Best Wood Tools<br></br>For Creative Work</h1>
-                            <p className="mb-5 lg:text-xl font-bold text-yellow-300">Hurry,Great Deals-Every Weekends</p>
+                            <p className="mb-5  lg:text-xl font-bold text-yellow-300">Hurry,Great Deals-Every Weekends</p>
                             <button className="btn-sm btn-primary lg:btn lg:btn-primary">Shop Now</button>
                         </div>
                     </div>
                 </div >
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide3" className="btn btn-circle">❮</a>
-                    <a href="#slide2" className="btn btn-circle">❯</a>
+                    <a href="#slide3" className="btn btn-circle w-4 -ml-5  lg:w-12">❮</a>
+                    <a href="#slide2" className="btn btn-circle w-4 -mr-5  lg:w-12">❯</a>
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
@@ -28,14 +28,14 @@ const Banner = () => {
                     <div className="hero-content text-center text-neutral-content">
                         <div className="max-w-md">
                             <h1 className="mb-5 text-3xl lg:text-5xl font-bold font-serif">Best Hand Saw Tools Collection</h1>
-                            <p className="mb-5 lg:text-xl font-bold text-yellow-300">Save 20% off - on your First Order</p>
+                            <p className="mb-5  lg:text-xl font-bold text-yellow-300">Save 20% off - on your First Order</p>
                             <button className="btn-sm lg:btn btn-primary lg:btn-primary">Shop Now</button>
                         </div>
                     </div>
                 </div >
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide1" className="btn btn-circle">❮</a>
-                    <a href="#slide3" className="btn btn-circle">❯</a>
+                    <a href="#slide1" className="btn btn-circle w-4 -ml-5  lg:w-12">❮</a>
+                    <a href="#slide3" className="btn btn-circle w-4 -mr-5  lg:w-12">❯</a>
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
@@ -50,8 +50,8 @@ const Banner = () => {
                     </div>
                 </div >
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide2" className="btn btn-circle">❮</a>
-                    <a href="#slide1" className="btn btn-circle">❯</a>
+                    <a href="#slide2" className="btn btn-circle w-4 -ml-5  lg:w-12">❮</a>
+                    <a href="#slide1" className="btn btn-circle w-4 -mr-5  lg:w-12">❯</a>
                 </div>
             </div>
         </div >
--- src/Pages/Home/Header.js
+++ src/Pages/Home/Header.js
@@ -36,14 +36,14 @@ const Header = () => {
         <div className="navbar bg-base-300 rounded-lg">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                    <label tabIndex="0" className="btn btn-ghost lg:hidden ">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 ">
                         {menuItems}
                     </ul>
                 </div>
-                <Link to='/' className="btn btn-link hover:no-underline normal-case text-2xl font-semibold text-orange-500 no-underline"><span className=' text-blue-700 uppercase font-serif'>daily</span>TOOLS</Link>
+                <Link to='/' className="btn btn-link hover:no-underline normal-case lg:text-2xl font-semibold text-orange-500 no-underline  p-0"><span className=' text-blue-700 uppercase font-serif'>daily</span>TOOLS</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
